d502e859b78a401f27e496dc5f3a0be03c2cbcdf
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2017-09-07 12:51:45 +0200

    server/test.js

---
+++ server/test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const bitcoin = require('bitcoinjs-lib');
+const BigInteger = require('bigi');
+
+const newBI = function (i) {
+	return BigInteger.valueOf(i);
+};
+
+const testRandomAddress = function () {
+	// for testing only
+	function rng () { return new Buffer('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz') }
+
+	// generate random keyPair
+	var keyPair = bitcoin.ECPair.makeRandom({ rng: rng })
+	var address = keyPair.getAddress()
+
+	assert.strictEqual(address, '1F5VhMHukdnUES9kfXqzPzMeF1GPHKiF64')
+	console.log ("bye bye");
+}
+
+
+testRandomAddress ();
