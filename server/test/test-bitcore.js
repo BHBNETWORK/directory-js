0414b4c7fda3ad268474dec3a28a23c3c88d156a
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2017-09-08 15:27:26 +0200

    xo ok

--- server/test/test-bitcore.js
+++ server/test/test-bitcore.js
@@ -1,61 +1,61 @@
 const assert = require('assert');
 const bitcore = require('bitcore-lib');
 
-var testBitcore = function (){
+const testBitcore = function () {
 	const kReferences = [
 		{
-			// the first at http://directory.io
-			index:'1',
-			wif:'5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf',
-			address:{
-				extended:'1EHNa6Q4Jz2uvNExL497mE43ikXhwF6kZm',
-				compressed:'1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH'
+			// The first at http://directory.io
+			index: '1',
+			wif: '5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf',
+			address: {
+				extended: '1EHNa6Q4Jz2uvNExL497mE43ikXhwF6kZm',
+				compressed: '1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH'
 			}
 		},
 		{
-			// just a random one at http://directory.io/123
-			index:'15710',
-			wif:'5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEssAj43uVyc',
-			address:{
-				extended:'1Ky6xofrbNDAg6nyrcKx2nD6AsvXhXeLmJ',
-				compressed:'18nQk4tQHfgcxoEzeFq8ceEUAANw7BYPko'
+			// Just a random one at http://directory.io/123
+			index: '15710',
+			wif: '5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEssAj43uVyc',
+			address: {
+				extended: '1Ky6xofrbNDAg6nyrcKx2nD6AsvXhXeLmJ',
+				compressed: '18nQk4tQHfgcxoEzeFq8ceEUAANw7BYPko'
 			}
 		},
 		{
-			// the last at http://directory.io/904625697166532776746648320380374280100293470930272690489102837043110636675
-			index:'115792089237316195423570985008687907852837564279074904382605163141518161494336',
-			wif:'5Km2kuu7vtFDPpxywn4u3NLpbr5jKpTB3jsuDU2KYEqetqj84qw',
-			address:{
-				extended:'1JPbzbsAx1HyaDQoLMapWGoqf9pD5uha5m',
-				compressed:'1GrLCmVQXoyJXaPJQdqssNqwxvha1eUo2E'
+			// The last at http://directory.io/904625697166532776746648320380374280100293470930272690489102837043110636675
+			index: '115792089237316195423570985008687907852837564279074904382605163141518161494336',
+			wif: '5Km2kuu7vtFDPpxywn4u3NLpbr5jKpTB3jsuDU2KYEqetqj84qw',
+			address: {
+				extended: '1JPbzbsAx1HyaDQoLMapWGoqf9pD5uha5m',
+				compressed: '1GrLCmVQXoyJXaPJQdqssNqwxvha1eUo2E'
 			}
 		}
 	];
-	kReferences.forEach (function (aReference){
+	kReferences.forEach(aReference => {
 		const aBN = bitcore.crypto.BN.fromString(aReference.index);
-		
-		// extended
-		const aPrivateKeyExtended = new bitcore.PrivateKey({bn:aBN, compressed:false, network:'livenet'});
-		const aWIFExtended = aPrivateKeyExtended.toWIF ();
-		const aAddressExtended = aPrivateKeyExtended.toAddress ();
 
-		// compressed
-		const aPrivateKeyCompressed = new bitcore.PrivateKey({bn:aBN, compressed:true, network:'livenet'});
-		const aWIFCompressed = aPrivateKeyCompressed.toWIF ();
-		const aAddressCompressed = aPrivateKeyCompressed.toAddress ();
+		// Extended
+		const aPrivateKeyExtended = new bitcore.PrivateKey({bn: aBN, compressed: false, network: 'livenet'});
+		const aWIFExtended = aPrivateKeyExtended.toWIF();
+		const aAddressExtended = aPrivateKeyExtended.toAddress();
+
+		// Compressed
+		const aPrivateKeyCompressed = new bitcore.PrivateKey({bn: aBN, compressed: true, network: 'livenet'});
+		const aWIFCompressed = aPrivateKeyCompressed.toWIF(); // eslint-disable-line no-unused-vars
+		const aAddressCompressed = aPrivateKeyCompressed.toAddress();
 
 		const aCalculatedValue = {
-			index:aReference.index,
-			wif:aWIFExtended.toString (),
-			address:{
-				extended:aAddressExtended.toString (),
-				compressed:aAddressCompressed.toString ()
+			index: aReference.index,
+			wif: aWIFExtended.toString(),
+			address: {
+				extended: aAddressExtended.toString(),
+				compressed: aAddressCompressed.toString()
 			}
 		};
-		console.log ({reference:aReference, calculated:aCalculatedValue});
-		assert.strictEqual (JSON.stringify (aReference), JSON.stringify (aCalculatedValue));
+		console.log({reference: aReference, calculated: aCalculatedValue});
+		assert.strictEqual(JSON.stringify(aReference), JSON.stringify(aCalculatedValue));
 	});
-	console.log ("test-bitcore-lib ok");	
-}
+	console.log('test-bitcore-lib ok');
+};
 
-testBitcore ();
+testBitcore();
