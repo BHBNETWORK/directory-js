96ee8d9c8b774a1357d6a0b48f8d24f5f49441d5
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2018-04-17 19:47:25 +0200

    client: showing page index

--- client/index.js
+++ client/index.js
@@ -60,11 +60,21 @@ let gController = null;
 					return aCalculatedValue;
 				}
 			};
+			self.buttonDisable = function (theButton) {
+				theButton.classList.remove('normal');
+				theButton.classList.add('busy');
+			};
+
+			self.buttonEnable = function (theButton) {
+				theButton.classList.remove('busy');
+				theButton.classList.add('normal');
+			};
 			self.model = {
 				bnPage: null // BN of current page.
 			};
-			self.onClickButton = theIncrement => {
+			self.onClickButton = (theButton, theIncrement) => {
 				return function () {
+					self.buttonDisable(theButton);
 					self.model.bnPage = self.model.bnPage.add(theIncrement);
 					setTimeout(self.showTable, 0);
 				};
@@ -75,20 +85,30 @@ let gController = null;
 				const bnPage = self.model.bnPage.sub(bnOne);
 				const bnBegin = bnPage.mul(bnDelta).add(bnOne);
 				const bnEnd = bnBegin.add(bnDelta);
-				const aDOMPageNumber = self.util.createElement('h2', {textContent: self.model.bnPage.toString ()});
-				const aPrevButton = self.util.createElement('span', {textContent: 'Prev', onclick: self.onClickButton(bnOne.neg())}, ['ig_button', 'normal']);
-				const aNextButton = self.util.createElement('span', {textContent: 'Next', onclick: self.onClickButton(bnOne)}, ['ig_button', 'normal']);
+				const bnMaxNumberOfIndex = bitcore.crypto.BN.fromString('115792089237316195423570985008687907852837564279074904382605163141518161494336');
+				const bnMaxPages = bnMaxNumberOfIndex.div(bnDelta);
+				const aDOMPageNumber = self.util.createElement('h2', {textContent: 'Page ' + self.model.bnPage.toString() + ' of ' + bnMaxPages.toString()});
+				const aDOMNumberOfIndex = self.util.createElement('h3', {textContent: 'Total: ' + bnMaxNumberOfIndex.toString()});
+				const aPrevButton = self.util.createElement('span', {textContent: 'Previous'}, ['ig_button', 'normal']);
+				aPrevButton.addEventListener('click', self.onClickButton(aPrevButton, bnOne.neg()));
+				const aNextButton = self.util.createElement('span', {textContent: 'Next'}, ['ig_button', 'normal']);
+				aNextButton.addEventListener('click', self.onClickButton(aNextButton, bnOne));
 				const aDOMTableWrapper = document.getElementById('tableWrapper');
 				aDOMTableWrapper.innerHTML = null;
-				[aDOMPageNumber, aPrevButton, aNextButton].forEach(theDOM => {
+				[aDOMPageNumber, aDOMNumberOfIndex, aPrevButton, aNextButton].forEach(theDOM => {
 					aDOMTableWrapper.appendChild(theDOM);
 				});
-				const results = [];
-				for (let bnIter = bnBegin; bnIter.lt(bnEnd); bnIter = bnIter.add(bnOne)) {
-					results.push(self.util.createAddressFromBNIndex(bnIter));
-				}
-				const aContent = self.util.createElement('pre', {textContent: JSON.stringify(results, null, 8)}, ['json']);
-				aDOMTableWrapper.appendChild(aContent);
+				setTimeout(() => {
+					const results = [];
+					for (let bnIter = bnBegin; bnIter.lt(bnEnd); bnIter = bnIter.add(bnOne)) {
+						results.push(self.util.createAddressFromBNIndex(bnIter));
+					}
+					const aContent = self.util.createElement('pre', {textContent: JSON.stringify(results, null, 8)}, ['json']);
+					aDOMTableWrapper.appendChild(aContent);
+					[aPrevButton, aNextButton].forEach(theButton => {
+						self.buttonEnable(theButton);
+					});
+				}, 0);
 			};
 
 			self.show = () => {
