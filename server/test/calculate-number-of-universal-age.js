53873bd66a637eccf3f71cf32233939b7056b738
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2019-03-09 18:19:56 +0100

    calculate-number-of-universal-age.js
    ```
    $ node server/test/calculate-number-of-universal-age.js | jq
    {
      "addresses": "115792089237316195423570985008687907852837564279074904382605163141518161494336",
      "globalHashingPower": "1000000000000000000000000000000000000",
      "years": "3669229891921952094695762193851494",
      "universeAge": "13820000000",
      "numberOfUniversalAge": "265501439357594218140069"
    }
    ```

---
+++ server/test/calculate-number-of-universal-age.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const bitcore = require('bitcore-lib');
+
+const main = () => {
+	// Number of Bitcoin P2PKH addresses
+	const aBNAddresses = bitcore.crypto.BN.fromString('115792089237316195423570985008687907852837564279074904382605163141518161494336');
+	const population = bitcore.crypto.BN.fromString('10000000000'); // 10 miliardi
+	const factorPerPerson = bitcore.crypto.BN.fromString('1000000000'); // 1 miliardo
+	const doubleHashRate = bitcore.crypto.BN.fromString('100000000000000000'); // 100 mila tera hash/s
+	const globalHashingPower = population.mul(factorPerPerson.mul(doubleHashRate));
+	const seconds = aBNAddresses.div(globalHashingPower);
+	const secondsPerYearBN = bitcore.crypto.BN.fromString(String(3600 * 24 * 365.25));
+	const years = seconds.div(secondsPerYearBN);
+	const universeAge = bitcore.crypto.BN.fromString(String(13.82 * 1000000000)); // https://it.wikipedia.org/wiki/Età_dell'universo
+	const numberOfUniversalAge = years.div(universeAge);
+	console.log(JSON.stringify({
+		addresses: aBNAddresses.toString(),
+		globalHashingPower: globalHashingPower.toString(),
+		years: years.toString(),
+		universeAge: universeAge.toString(),
+		numberOfUniversalAge: numberOfUniversalAge.toString()
+	}));
+};
+
+main();
